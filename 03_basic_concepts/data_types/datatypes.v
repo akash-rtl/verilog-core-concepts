86567de3e4c4695009ca842118111c9e0976a1ed
Akash Mishra <omakash2003@gmail.com>
2026-05-03 22:17:31 +0530

    edited the code

--- 03_basic_concepts/data_types/datatypes.v
+++ 03_basic_concepts/data_types/datatypes.v
@@ -1,5 +1,19 @@
 //value set: 0,1,z,x
 
 module datatype;
+    //1. nets
+    wire net1; 
 
+    //2. registers
+    reg register1 = 1'b1;
+
+    //3. Vectors
+    reg [7:0] bus; //8-bit bus
+
+
+
+
+    initial begin
+        $display(net1, register1);
+    end
 endmodule
