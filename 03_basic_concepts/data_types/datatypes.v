e81a3f9b2c99cf84c69a889dacbaec1261947277
Akash Mishra <omakash2003@gmail.com>
2026-05-04 08:14:57 +0530

    added new datatypes

--- 03_basic_concepts/data_types/datatypes.v
+++ 03_basic_concepts/data_types/datatypes.v
@@ -20,7 +20,34 @@ module datatype;
     //6. Time
     time save_sim_time;
 
+    //7. Array
+    reg bool_arr [31:0]; // 32 one bit boolean register variable
+	wire [7:0] w_arr [5:0]; //declare an array of 8 bit vector wire 
+	reg [63:0] array_4d [15:0][7:0][7:0][255:0]; // Four dimensional array
+
+    initial 
+        array_4d[0][0][0][0][15:0] = 0; //clr bit 15:0 for the register accessed by [0][0][0][0]
+
+    //8. Memories: 1-dimensional array
+    reg mem1bit [0:1023]; //Memory mem1bit with 1K 1-bit words
+    reg [7:0] membyte [0:1023]; //Memory membyte with 1K 8-bit words(bytes)
     
+    //to fetch one:
+    initial 
+        membyte[511] = 0; //here entire 1byte word is fetched
+
+    
+    //9. Parameters - using defparam they can be modified, will discuss later in detail
+    parameter port_id = 5;
+    //parameter signed [15:0] WIDTH; //fixed sign and range for parameter width
+
+    //10. Strings - each char takes 8-bit
+    reg [8*18:1] string_value;
+    
+    initial begin  
+        string_value = "Hello Verilog World";
+        $display("%s",string_value);
+    end
 
     
     
@@ -39,4 +66,6 @@ module datatype;
         save_sim_time = $time;
         $displayb(net1, " ", register1," %0b", bus, " ", byte_select," %d", save_sim_time);
     end
+
+
 endmodule
