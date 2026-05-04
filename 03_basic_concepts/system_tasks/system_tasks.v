49386e60990498e5553b9a441a4a6b2a35d1a58c
Akash Mishra <omakash2003@gmail.com>
2026-05-04 10:21:37 +0530

    editin done

--- 03_basic_concepts/system_tasks/system_tasks.v
+++ 03_basic_concepts/system_tasks/system_tasks.v
@@ -10,23 +10,21 @@
 
 
 module system_tasks;
-    initial begin
-    $dumpfile("system_tasks.vcd");
-    $dumpvars(0,system_tasks);
-    end
+    
     initial
         $display("Hello Verilog World!!");
 
-    reg [4:0] var = 25;
+    reg [4:0] var = 8;
     initial
         $display("Binary value of var is: %b",var);
 
     //$monitor
 
     reg clk = 0;
-    always
+    always begin
         #10 clk = ~clk;
-
+        var = var + 1;
+    end
     initial begin
         $monitor($time, " Clk value is: %b -- var value is: %b", clk, var);
 
@@ -34,6 +32,14 @@ module system_tasks;
         #100 $finish;
     end  
     
+
+
+
+    //let us get the graph for above vars:
+    initial begin
+    $dumpfile("system_tasks.vcd");
+    $dumpvars(0,system_tasks);
+    end
     
 
 endmodule
